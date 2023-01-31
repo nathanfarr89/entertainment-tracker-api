e61da40a52fbbddb8b261eeca6276153849f8984
Nathan Farr <nathanfarr@Nathans-MacBook-Pro.local>
2023-01-31 16:14:57 -0700

    update port'

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import { schema } from "./schema";
 // Create a Yoga instance with a GraphQL schema.
 const yoga = createYoga({ schema });
 
+const port = Number(process.env.API_PORT) || 4000;
+
 // Pass it into a server to hook into request handlers.
 const server = createServer(yoga);
 
 // Start the server and you're done!
-server.listen(4000, () => {
+server.listen(port, () => {
   console.info("Server is running on http://localhost:4000/graphql");
 });
